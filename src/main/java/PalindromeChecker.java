1417dc5b23cfa2010038add51a3fd41d6f04f7d1
matthewIeng <143155027+matthewIeng@users.noreply.github.com>
2024-02-02 06:55:04 -0800

    Update PalindromeChecker.java

--- src/main/java/PalindromeChecker.java
+++ src/main/java/PalindromeChecker.java
@@ -37,13 +37,16 @@ public void tester()
 }
 public boolean palindrome(String word)
 {
-  //your code here
+   if(reverse(word).equals(word))
+  return true;
   return false;
 }
 public String reverse(String str)
 {
     String sNew = new String();
-    //your code here
+    for(int i = str.length()-1; i >= 0; i--) {
+      sNew = sNew + str.substring(i,i+1);
+    }
     return sNew;
 }
 }
